537c7c6255e3b12f20b7303961b725a772b7f354
yash <devyashops@gmail.com>
2021-03-19 12:12:02 +0000

    Commit all files

--- demo1.java
+++ demo1.java
@@ -2,7 +2,8 @@ class demo1
 {
 	public static void main(String args[])
 		{
-			System.out.println("Java demo1 class"):
+			System.out.println("Java demo1 class");
+			System.out.println("Second line");
 		}
 }
 
