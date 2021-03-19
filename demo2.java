29c73241e03df10c59ad9d6e22d2ced37d35a2ff
yash <devyashops@gmail.com>
2021-03-19 13:01:02 +0000

    new changes to demo2

--- demo2.java
+++ demo2.java
@@ -3,6 +3,9 @@ class demo2
 	public static void main(String args[])
 	{
 		System.out.println("Java demo2 class");
+		System.out.println("Second line");
+		System.out.println("Third line");
 	}
+
 }
 
